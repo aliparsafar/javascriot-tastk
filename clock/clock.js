a79cb467ca332445140923b23fce5fdac7214303
alie <aliparsafar.code@gmail.com>
2025-01-29 18:36:28 +0330

    Clock project ended

---
+++ clock/clock.js
@@ -0,0 +1,11 @@
+function is12hour(hour){
+    if(hour===0){
+        console.log('12')
+    } else if( hour <=12){
+        console.log( hour)
+    }else if(hour >13 && hour<24){
+        console.log((hour-12 ))
+    }
+}
+console.log(is12hour(15))
+
